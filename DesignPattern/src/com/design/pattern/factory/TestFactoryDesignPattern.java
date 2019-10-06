d7f8847cf76d5ff94da8207d64b10443a38061bd
Sanjeev Rai <sanjeev.rai@u2opiamobile.com>
2019-10-06 06:22:13 +0530

    test case added for desktop

--- DesignPattern/src/com/design/pattern/factory/TestFactoryDesignPattern.java
+++ DesignPattern/src/com/design/pattern/factory/TestFactoryDesignPattern.java
@@ -3,10 +3,13 @@ package com.design.pattern.factory;
 public class TestFactoryDesignPattern {
 
 	public static void main(String args[]) {
-		
+
 		Computer laptop = ComputerFactory.createComputer("Laptop");
 		System.out.println(laptop);
-		
+
+		Computer desktop = ComputerFactory.createComputer("Desktop");
+		System.out.println(desktop);
+
 	}
 
 }
